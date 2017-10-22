bf479f2f976e6139af893aee585233915b2dcf6f
Ann Gould <gould.ann8@gmail.com>
2017-10-22 04:46:59 -0500

    some front fixes :)

--- front/script.js
+++ front/script.js
@@ -1,5 +1,17 @@
+var temp = 1;
 
+function printOut(){
+	var tempWindow = window.open("", "MsgWindow", "width=400,height=300");
+    tempWindow.document.write("<p>put in new, amazing data here!</p>");
+}
 
-$("#this").on('click', function() {
-	alert('this worked');
-});
+function createField(){
+	if(temp != 0){
+		document.getElementById('outputFieldId').style.display='block';
+		temp = 0;
+	}
+    else{
+    	temp = 1;
+    	document.getElementById('outputFieldId').style.display='none';
+    }
+}
