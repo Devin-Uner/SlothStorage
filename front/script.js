adb2e2e636fdcd5a572d0581680db03364c1eeaa
Ann Gould <gould.ann8@gmail.com>
2017-10-22 05:13:45 -0500

    oh lordie

--- front/script.js
+++ front/script.js
@@ -1,10 +1,5 @@
 var temp = 1;
 
-function printOut(){
-	var tempWindow = window.open("", "MsgWindow", "width=400,height=300");
-    tempWindow.document.write("<p>put in new, amazing data here!</p>");
-}
-
 function createField(){
 	if(temp != 0){
 		document.getElementById('outputFieldId').style.display='block';
